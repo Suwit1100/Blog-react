ab506978bcdf57e131d7ca50d9dcc984c83be22f
สุวิทย์ ลีเจ้ยวาระ <139098129+Suwit1100@users.noreply.github.com>
2024-05-09 15:44:30 +0700

    ดึงข้อมูล

--- blog/src/components/Detail.jsx
+++ blog/src/components/Detail.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import blogs from '../data/blogs';
 
 function Detail() {
-    const {id} = useParams();
+    const { id } = useParams();
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [image, setImage] = useState("");
+    const [author, setAuthor] = useState("");
+
+    useEffect(() => {
+        const result = blogs.find((item) => item.id === parseInt(id))
+        console.log(result);
+        setTitle(result.title);
+        setContent(result.content);
+        setImage(result.image_url);
+        setAuthor(result.author);
+    }, [])
     return (
         <div>
             บทความที่ {id}
