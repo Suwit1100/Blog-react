3512dde2bcea06a35067b658e98c2b8b370b16ff
สุวิทย์ ลีเจ้ยวาระ <139098129+Suwit1100@users.noreply.github.com>
2024-05-10 16:26:08 +0700

    search ข้อมูล

--- blog/src/components/Blogs.jsx
+++ blog/src/components/Blogs.jsx
@@ -1,24 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../css/Blogs.css'
 import { Link } from 'react-router-dom';
 function Blogs(props) {
     const { blogs } = props;
+    const [search, setSearch] = useState('');
+    const [filtersearch, setFiltersearch] = useState([]);
+    useEffect(() => {
+        if (search) {
+            const result = blogs.filter((item) => item.title.includes(search))
+            setFiltersearch(result)
+            console.log(filtersearch);
+        }
+    }, [search])
     return (
         <div className="blogs-container">
             <div className="search-container">
-                <input type="text" />
+                <input type="text" placeholder='ค้นหาบทความ' onChange={(e) => setSearch(e.target.value)} value={search} />
             </div>
-            <div className="result-search">
-                <div className="item">
-                    <img src="" alt="" width={50} height={50} />
-                    <span>adasda</span>
-                </div>
-                <hr />
-                <div className="item">
-                    <img src="" alt="" width={50} height={50} />
-                    <span>adasda</span>
-                    <hr />
-                </div>
+            <div className="result-search" hidden={!search}>
+                {filtersearch.length === 0 ? (
+                    <div className="item">
+                        <span className='notfound'>ไม่พบข้อมูล</span>
+                    </div>
+                ) : (
+                    filtersearch.map((e) => {
+                        return (
+                            <>
+                                <Link className="item" key={e.id} to={`/blog/${e.id}`}    >
+                                    <img src={e.image_url} alt={e.title} width={50} height={50} />
+                                    <span>{e.title}</span>
+                                </Link>
+                            </>
+                        );
+                    })
+                )}
+
             </div>
             <article>
                 {blogs.map((item) => {
